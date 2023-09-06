1713d95f0a3ca915a65cbeaed99eab7694702557
amiribnjaman <amiribnjaman@gmail.com>
2023-09-06 16:21:13 +0600

    Unnecessary code erased

--- commonFunctions/sorting_asc_des.js
+++ commonFunctions/sorting_asc_des.js
@@ -11,22 +11,4 @@ const sorting_asc_des =(order, products_to_sort)=> {
     return sorted_products
 }
 
-
-// const sorting_asc_des =(order, products_to_sort)=> {
-//     let sorted_products = []
-//     if(order.includes('aes')){
-//         console.log(order.includes('aes'))
-//         sorted_products = products_to_sort.sort((a,b)=> {
-//             return parseInt(a.price) - parseInt(b.price);
-//           });
-//     } else if (order.includes('des')){
-//         console.log(order.includes('dees'))
-//         sorted_products = products_to_sort.sort((a,b)=> {
-//             return parseInt(b.price) - parseInt(a.price);
-//          });
-//     }
-
-//     return sorted_products
-// }
-
 module.exports = sorting_asc_des
